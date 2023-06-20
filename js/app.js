b8726a85a5ce0b763f3f54fef9004594fa4993b3
alejandromedranojr <126729332+alejandromedranojr@users.noreply.github.com>
2023-06-19 22:30:17 -0600

    App completa

---
+++ js/app.js
@@ -0,0 +1,79 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'auto',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/hombres/',
+    	url: 'hombres.html',
+    	name: 'hombres',
+  		},
+	],
+	dialog: {
+		title: 'Adidas',
+		buttonOk: 'Aceptar',
+		buttonCancel: 'Cancelar'
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('!Welcome to AppBase !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnComprar_', function (e) {
+	e.preventDefault();
+
+
+	
+});
+
+
+
+
+
